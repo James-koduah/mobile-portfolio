a9afe93a698fb002b7ee98f01e33d17f79613761
James-koduah <jameskoduah171@gmail.com>
2023-02-22 12:42:20 +0000

    minor changes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let about = document.getElementById("about")
 let project = document.getElementById("projects")
 let nav = document.getElementById("nav")
 let div_clear = document.getElementById("div_clear")
+let nav_drop_button = document.getElementById("nav_drop")
 current = home
 function change_Page(page){
     if (page == "about"){
@@ -30,11 +31,15 @@ function nav_drop(){
     if (b == false)
     {
         nav.style.zIndex = "19";
+        nav_drop_button.style.background = "url(images/side.png)"
+        nav_drop_button.style.backgroundSize = "100% 100%"
         div_clear.style.zIndex = "19"
         b = true 
     }
     else{
         nav.style.zIndex = "0";
+        nav_drop_button.style.background = "url(images/iconmonstr-text-23-240.png)"
+        nav_drop_button.style.backgroundSize = "100% 100%"
         div_clear.style.zIndex = "0"
         b = false
     }
